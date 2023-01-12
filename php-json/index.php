4df1aaa0a97f325835fb6708da708fb996dabf1c
{hauvi} <{stdhauvi@gmail.com}>
2023-01-12 11:04:00 +0700

    t1

--- php-json/index.php
+++ php-json/index.php
@@ -17,6 +17,14 @@
         <hr><br>
         <label for="pa_title">Parent title:</label>
 
+        <?php
+          $data = file_get_contents('phuyen/sidebarConf/sidebar_channuoi.json');
+          $datasDecoded = json_decode($data, true);
+          
+          $pa_title = $datasDecoded['channuoi']['pContent']['pChildContent'][0]['title'];
+
+          echo json_encode($datasDecoded, JSON_UNESCAPED_UNICODE);
+        ?>
         <select name="pa_title" id="pa_title">
             <option value="">--- Select parent title ---</option>
             <option value="Dịch bệnh chăn nuôi">Dịch bệnh chăn nuôi</option>
@@ -30,17 +38,22 @@
         <input type="text" id="title" name="title"><br><br>
         <label for="map_url">Map url:</label>
         <input type="text" id="map_url" name="map_url"><br><br>
+        <label for="note_url">Note url:</label>
+        <input type="text" id="note_url" name="note_url"><br><br>
         <input type="submit" value="Add" name="add">
-        <input type="submit" value="Edit" name="edit"><br><br>
+        <input type="submit" value="Edit" name="edit">
+        <input type="submit" value="Del" name="del"><br><br>
         <hr><br>
     </form>
     <?php
+    //add data
     if (isset($_GET['add'])) {
-        if ((isset($_GET['id']) && $_GET['title'] && $_GET['map_url'] && $_GET['pa_title']) && !empty($_GET['id']) && $_GET['title'] && $_GET['map_url'] && $_GET['pa_title']) {
+        if ((isset($_GET['id']) && $_GET['pa_title'] && $_GET['map_url'] && $_GET['title'] || $_GET['note_url']) && !empty($_GET['id']) && $_GET['title'] && $_GET['map_url'] && $_GET['pa_title'] || $_GET['note_url']) {
             $id = $_GET['id'];
             $title = $_GET['title'];
             $map_url = $_GET['map_url'];
             $pa_title = $_GET['pa_title'];
+            $note_url = $_GET['note_url'];
             // $datas = file_get_contents('data.json');
             $data_sidebar = file_get_contents('phuyen/sidebarConf/sidebar_channuoi.json');
             $data_map = file_get_contents('phuyen/mapConf/mapwms.json');
@@ -58,14 +71,14 @@
                 $level = 2;
             }
 
-            $chudan = 'https://nongnghiepphuyen.girs.vn/file_system/a1_mapicon/icon_channuoi_mobile_dichbenh_min.png';
+            // $chudan = 'https://nongnghiepphuyen.girs.vn/file_system/a1_mapicon/icon_channuoi_mobile_dichbenh_min.png';
             $isEx = false;
             $isSelect = false;
             $pChildContent = [];
 
             // Create Array to json file for Add data  
             // $datasDecoded[0]['member'][] = ["id" => $id, "title" => $title, "map_url" => $map_url];
-            $data_sidebar_Decoded['channuoi']['pContent']['pChildContent'][$level]['pChildContent'][] = ["title" => $title, "id" => $id, "chudan" => $chudan, "isEx" => $isEx, "isSelect" => $isSelect, "pChildContent" => $pChildContent];
+            $data_sidebar_Decoded['channuoi']['pContent']['pChildContent'][$level]['pChildContent'][] = ["title" => $title, "id" => $id, "chudan" => $note_url, "isEx" => $isEx, "isSelect" => $isSelect, "pChildContent" => $pChildContent];
             $data_map_Decoded[$id] = ["iURL4Map" => $map_url];
 
             //Encode the array back into a JSON string.
@@ -79,13 +92,15 @@
             file_put_contents('phuyen/sidebarConf/sidebar_channuoi.json', $json_sidebar);
             file_put_contents('phuyen/mapConf/mapwms.json', $json_map);
         }
-    } elseif (isset($_GET['edit'])) {
-        if ((isset($_GET['id']) && $_GET['title'] && $_GET['map_url'] && $_GET['pa_title']) && !empty($_GET['id']) && $_GET['title'] && $_GET['map_url'] && $_GET['pa_title']) {
+    }
+    // edit data
+    elseif (isset($_GET['edit'])) {
+        if ((isset($_GET['id']) && $_GET['title'] && $_GET['map_url'] && $_GET['pa_title'] && $_GET['note_url']) && !empty($_GET['id']) && $_GET['title'] && $_GET['map_url'] && $_GET['pa_title'] && $_GET['note_url']) {
             $id = $_GET['id'];
             $title = $_GET['title'];
             $map_url = $_GET['map_url'];
             $pa_title = $_GET['pa_title'];
-
+            $note_url = $_GET['note_url'];
             // read file
             // $data = file_get_contents('data.json');
             $data_sidebar = file_get_contents('phuyen/sidebarConf/sidebar_channuoi.json');
@@ -103,39 +118,106 @@
             $isEx = false;
             $isSelect = false;
             $pChildContent = [];
-
+            $i = -1;
             // decode json to array
             $data_sidebar_Decoded = json_decode($data_sidebar, true);
             $data_map_Decoded = json_decode($data_map, true);
-            
-            foreach ($data_sidebar_Decoded['channuoi']['pContent']['pChildContent'][$level]['pChildContent'] as $key => $value) {
-                if ($value['id'] == $id) {
-                    $data_sidebar_Decoded[$key]['channuoi']['pContent']['pChildContent'][$level]['pChildContent']['title'] = $title;
-                    echo 'hauvi';
-                //     // $data_sidebar_Decoded[$key]['id'] = $id;
-                //     $data_sidebar_Decoded[$key][0]['channuoi']['pContent']['pChildContent'][$level]['pChildContent']['chudan'] = $chudan;
-                //     $data_sidebar_Decoded[$key][0]['channuoi']['pContent']['pChildContent'][$level]['pChildContent']['isEx'] = $isEx;
-                //     $data_sidebar_Decoded[$key][0]['channuoi']['pContent']['pChildContent'][$level]['pChildContent']['isSelect'] = $isSelect;
-                //     $data_sidebar_Decoded[$key][0]['channuoi']['pContent']['pChildContent'][$level]['pChildContent']['pChildContent'] = $pChildContent;
-                }
 
+
+
+            foreach ($data_sidebar_Decoded['channuoi']['pContent']['pChildContent'][$level]['pChildContent'] as $key => $value) {
+                $i++;
+                if (strcmp($value['id'], $id) == 0) {
+                    $data_sidebar_Decoded['channuoi']['pContent']['pChildContent'][$level]['pChildContent'][$i]['title'] = $title;
+                    $data_sidebar_Decoded['channuoi']['pContent']['pChildContent'][$level]['pChildContent'][$i]['id'] = $id;
+                    $data_sidebar_Decoded['channuoi']['pContent']['pChildContent'][$level]['pChildContent'][$i]['chudan'] = $note_url;
+                }
             }
 
+
             // foreach ($data_map_Decoded as $key => $value) {
-            //     if ($value[$title] == $title) {
-            //         $data_map_Decoded[$key][0]['iURL4Map'] = $map_url;
+            //     $i++;
+
+            //     if ($value == $id) {
+            //         $data_map_Decoded[$i]['iURL4Map'] = $map_url;
+            //         // $data_map_Decoded[$key] = $id;
             //     }
+
             // }
+            // var_dump($tmp_map_decode);
 
             $json_sidebar = json_encode($data_sidebar_Decoded, JSON_UNESCAPED_UNICODE);
             echo $json_sidebar;
             echo '<hr>';
-            $json_map = json_encode($data_map_Decoded, JSON_UNESCAPED_UNICODE);
-            echo $json_map;
-            echo '<hr>';
+            // $json_map = json_encode($data_map_Decoded, JSON_UNESCAPED_UNICODE);
+            // echo $json_map;
+            // echo '<hr>';
             // encode array to json and save to file
             file_put_contents('phuyen/sidebarConf/sidebar_channuoi.json', $json_sidebar);
-            file_put_contents('phuyen/mapConf/mapwms.json', $json_map);
+            // file_put_contents('phuyen/mapConf/mapwms.json', $json_map);
+        }
+    }
+    // delete data
+    elseif (isset($_GET['del'])) {
+        if ((isset($_GET['id']) && $_GET['title'] && $_GET['map_url'] && $_GET['pa_title'] && $_GET['note_url']) && !empty($_GET['id']) && $_GET['title'] && $_GET['map_url'] && $_GET['pa_title'] && $_GET['note_url']) {
+            $id = $_GET['id'];
+            $title = $_GET['title'];
+            $map_url = $_GET['map_url'];
+            $pa_title = $_GET['pa_title'];
+            $chudan = 'https://nongnghiepphuyen.girs.vn/file_system/a1_mapicon/icon_channuoi_mobile_dichbenh_min.png';
+            $isEx = false;
+            $isSelect = false;
+            $pChildContent = [];
+            
+            if ($pa_title == 'Dịch bệnh chăn nuôi') {
+                $level = 0;
+            } elseif ($pa_title == 'Cơ sở sản xuất chăn nuôi') {
+                $level = 1;
+            } elseif ($pa_title == 'Thống kê chăn nuôi') {
+                $level = 2;
+            }
+
+            // read file
+            $data_sidebar = file_get_contents('phuyen/sidebarConf/sidebar_channuoi.json');
+            $data_map = file_get_contents('phuyen/mapConf/mapwms.json');
+            // decode json to array
+            $data_sidebar_Decoded = json_decode($data_sidebar, true);
+            $data_map_Decoded = json_decode($data_map, true);
+            // get array index to delete
+            $arr_index_sidebar = [];
+            $arr_index_map = array();
+            foreach ($data_sidebar_Decoded['channuoi']['pContent']['pChildContent'][$level]['pChildContent'] as $key => $value) {
+                if ($value['id'] == $id) {
+                    $arr_index_sidebar[] = $key;
+                }
+            }
+            // foreach ($data_map_Decoded as $key => $value) {
+            //     if ($value['id'] == $id) {
+            //         $arr_index_map[] = $key;
+            //     }
+            // }
+
+
+            // delete data
+            foreach ($arr_index_sidebar as $i) {
+                unset($data_sidebar_Decoded['channuoi']['pContent']['pChildContent'][$level]['pChildContent'][$i]);
+            }
+            // foreach ($arr_index_map as $i) {
+            //     unset($data_map_Decoded[$i]);
+            // }
+
+           
+          
+            $json_sidebar = json_encode($data_sidebar_Decoded, JSON_UNESCAPED_UNICODE);
+            // $data_map_Decoded = array_values($data_map_Decoded);
+            // $json_map = json_encode($data_map_Decoded, JSON_UNESCAPED_UNICODE);
+            // encode array to json and save to file
+            file_put_contents('phuyen/sidebarConf/sidebar_channuoi.json', $json_sidebar);
+            // file_put_contents('phuyen/mapConf/mapwms.json', $json_map);
+            echo $json_sidebar;
+            echo '<hr>';
+             // echo $json_map;
+            // echo '<hr>';
         }
     }
 
@@ -148,7 +230,7 @@
         $datasDecoded = json_decode($data, true);
         // echo $datasDecoded[1]["member"][3]["name"];
         // echo json_encode($datasDecoded);
-        // var_dump($data)
+        // var_dump($datasDecoded)
         // foreach ($datasDecoded['channuoi']['pContent']['pChildContent'][1]['pChildContent'] as $key => $value) {
         //     // if ($value['id'] == 'channuoi_coso_nhayen') {
         //         $data_sidebar_Decoded[$key][0]['channuoi']['pContent']['pChildContent']['pChildContent']['title'] = "hauvi";
--- php-json/testphp.php
+++ php-json/testphp.php
@@ -1,5 +1,5 @@
 <?php
-// read file
+/* // read file
 $data = file_get_contents('data.json');
 
 // decode json to array
@@ -14,5 +14,37 @@ foreach ($json_arr[0]['member'] as $key => $value) {
 // encode array to json and save to file
 file_put_contents('data.json', json_encode($json_arr));
 $json = json_encode($json_arr);
-echo $json;
+echo $json; */
+?>
+
+<?php
+// read json file
+$data = file_get_contents('data.json');
+
+// decode json to associative array
+$json_arr = json_decode($data, true);
+
+// get array index to delete
+$arr_index = array();
+foreach ($json_arr[0]['member'] as $key => $value)
+{
+    if ($value['id'] == '1')
+    {
+        $arr_index[] = $key;
+    }
+}
+
+// delete data
+foreach ($arr_index as $i)
+{
+    unset($json_arr[0]['member'][$i]);
+}
+
+// rebase array
+$json_arr = array_values($json_arr);
+
+// encode array to json and save to file
+file_put_contents('data.json', json_encode($json_arr));
+
+echo json_encode($json_arr);
 ?>
